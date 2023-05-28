a47e494c52c64f4688a12648c58aa0c1b2d0fc69
zonyitoo <zonyitoo@gmail.com>
2023-05-28 09:28:32 +0800

    make KCP_OVERHEAD in lib

--- src/lib.rs
+++ src/lib.rs
@@ -11,11 +11,11 @@ mod kcp;
 
 /// The `KCP` prelude
 pub mod prelude {
-    pub use super::{get_conv, Kcp};
+    pub use super::{get_conv, Kcp, KCP_OVERHEAD};
 }
 
 pub use error::Error;
-pub use kcp::{get_conv, get_sn, set_conv, Kcp};
+pub use kcp::{get_conv, get_sn, set_conv, Kcp, KCP_OVERHEAD};
 
 /// KCP result
 pub type KcpResult<T> = Result<T, Error>;
